b98b1fc6ecbf47ccb085217002ef17c39cf14eeb
lehuubao1810 <lehuubao2909@gmail.com>
2024-03-24 01:49:50 +0700

    Add Redux persist integration to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 import App from "./App.tsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./redux/store.ts";
+import {store, persistor} from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+    </PersistGate>
     {/* </React.StrictMode> */}
   </Provider>
 );
