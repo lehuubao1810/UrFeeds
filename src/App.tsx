859c08d0275d9e7c44f004b409ebbf093b9c714a
lehuubao1810 <lehuubao2909@gmail.com>
2024-03-24 01:50:06 +0700

    Refactor routing logic and implement authentication in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import { RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  // Navigate,
+} from "react-router-dom";
 
-import { router } from "./router";
+import { useSelector } from "react-redux";
+
+import NotFoundPage from "./pages/404";
+import LoginPage from "./pages/Login";
+import HomePage from "./pages/Home";
+import RegisterPage from "./pages/Register";
+import AddFeedPage from "./pages/AddFeed";
+import AddRss from "./pages/AddRss";
+import ImportFile from "./pages/ImportFile";
+
+import { RootState } from "./redux/store";
 
 export default function App() {
-  // const isAuthenticated = false; // Thay đổi thành trạng thái đăng nhập thực tế
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  console.log(isAuthenticated);
 
   return (
     <>
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />,
+          <Route path="/login"  element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}/>,
+          <Route path="/register" element={isAuthenticated ? <Navigate to="/" /> : <RegisterPage />}/>,
+          <Route path="/" element={!isAuthenticated ?  <Navigate to="/login" /> : <HomePage/>}>
+            <Route path="/add" element={<AddFeedPage />}>
+              <Route path="/add/rss" element={<AddRss />} />
+              <Route path="/add/import" element={<ImportFile />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
